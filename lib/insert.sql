207fcca910be62e4c5c9db06bc15b22aece14904
Abdela Umer <adheremo65@gmail.com>
2023-07-29 17:04:02 -0400

    sql quires

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive)
+VALUES 
+("Mr.Chocolate",2,"M","red","wow",1),
+("Rowdy",2,"M","red","wow",1),
+("Tabitha",2,"M","red","wow",1),
+("Sergeant Brown",2,"F","red","wow",1),
+("Melissa",2,"F","red","wow",1),
+("Grinch",2,"F","red","wow",1),
+("Wendey",2,"F","red","wow",1),
+(NULL,2,"F","red","wow",1);
